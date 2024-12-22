471273aa0f5885765f3e0b04338a2167d9e0f5b6
dong <2389314853@qq.com>
2024-12-22 09:07:33 +0000

    add reatapp

--- tasky-api/eslint.config.mjs
+++ tasky-api/eslint.config.mjs
@@ -1,18 +1,21 @@
 import globals from "globals";
 import pluginJs from "@eslint/js";
 
-/** @type {import('eslint').Linter.Config[]} */
-export default [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      globals: globals.node,
-      ecmaVersion: "latest", // 支持最新的 ECMAScript
-      sourceType: "module",  // 使用 ECMAScript 模块
+/** @type {import('eslint').Linter.Config} */
+export default {
+  overrides: [
+    {
+      files: ["**/*.js"],
+      languageOptions: {
+        globals: globals.node,
+        ecmaVersion: 2015, // ES6
+        sourceType: "module", // Use ES Module format
+      },
+      rules: {
+        semi: ["warn", "always"], // 警告模式：确保语句以分号结束
+        "no-console": "off",     // 允许使用 console
+      },
     },
-    rules: {
-      "semi": 1,            // 警告：缺少分号
-      "no-console": "off",  // 允许 console.log
-    },
-  },
-];
+  ],
+  ...pluginJs.configs.recommended, // 使用 @eslint/js 推荐的配置
+};
