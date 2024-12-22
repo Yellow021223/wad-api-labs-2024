adeaea4b651c0b6f7441bcd0798f61df140e4d60
dong <2389314853@qq.com>
2024-12-22 09:43:33 +0000

    Integration Lab - Get Movies from API

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,9 +1,14 @@
 export const getMovies = async () => {
-    const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=58ac426817dbdd46e759d1ae7cc7bbb3&language=en-US&include_adult=false&page=1`
-    )
-    return response.json()
-  };
+  const response = await fetch(
+    'http://localhost:8080/api/movies', {
+    headers: {
+      'Authorization': window.localStorage.getItem('token')
+    }
+  }
+  )
+  return response.json();
+};
+
   export const login = async (username, password) => {
     const response = await fetch('http://localhost:8080/api/users', {
         headers: {
