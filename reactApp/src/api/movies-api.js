77fea30bdfdf23bc751918885a22b269419b8141
dong <2389314853@qq.com>
2024-12-22 09:39:00 +0000

    Integration Lab - Integrate Users Endpoints

--- reactApp/src/api/movies-api.js
+++ reactApp/src/api/movies-api.js
@@ -1,6 +1,27 @@
 export const getMovies = async () => {
     const response = await  fetch(
-      `https://api.themoviedb.org/3/discover/movie?api_key=c1gfdg225ecfffffffff3612d8c1bcb4bcb&language=en-US&include_adult=false&page=1`
+      `https://api.themoviedb.org/3/discover/movie?api_key=58ac426817dbdd46e759d1ae7cc7bbb3&language=en-US&include_adult=false&page=1`
     )
     return response.json()
   };
+  export const login = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch('http://localhost:8080/api/users?action=register', {
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        method: 'post',
+        body: JSON.stringify({ username: username, password: password })
+    });
+    return response.json();
+};  
