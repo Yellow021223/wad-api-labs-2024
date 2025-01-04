96e4ff07bf2c88e3b12c55374340d1763b0bc0c9
dong <2389314853@qq.com>
2025-01-04 22:25:52 +0000

    two of the API endpoints called from frontend

--- reactApp/src/pages/moviesPage.js
+++ reactApp/src/pages/moviesPage.js
@@ -1,22 +1,58 @@
 import { useQuery } from 'react-query';
-import { getMovies } from "../api/movies-api";
+import { getMovies, getUpcomingMovies } from "../api/movies-api";
 
 const MoviesPage = () => {
-    const { data, error, isLoading, isError } = useQuery('discover', getMovies)
-    if (isLoading) {
-        return <h1>Loading...</h1>
+    const { data: moviesData, error: moviesError, isLoading: moviesLoading, isError: moviesIsError } = useQuery('discover', getMovies);
+    const { data: upcomingData, error: upcomingError, isLoading: upcomingLoading, isError: upcomingIsError } = useQuery('upcoming', getUpcomingMovies);
+
+    if (moviesLoading || upcomingLoading) {
+        return <h1>Loading...</h1>;
     }
 
-    if (isError) {
-        return <h1>{error.message}</h1>
+    if (moviesIsError) {
+        return <h1>{moviesError.message}</h1>;
     }
-    const movies = data.results;
+
+    if (upcomingIsError) {
+        return <h1>{upcomingError.message}</h1>;
+    }
+
+    const movies = moviesData.results || [];
+    const upcomingMovies = upcomingData.results || [];
+
     const moviesDisplay = (
         <div>
-            {movies.map(movie => { return <li key={movie.id}>{movie.id},{movie.title}<br /></li> })}
+            <h3>All Movies</h3>
+            <ul>
+                {movies.map(movie => (
+                    <li key={movie.id}>
+                        {movie.id}, {movie.title}
+                    </li>
+                ))}
+            </ul>
         </div>
-    )
-    return <div><h2>Movies</h2>{moviesDisplay}</div>
-}
+    );
+
+    const upcomingMoviesDisplay = (
+        <div>
+            <h3>Upcoming Movies</h3>
+            <ul>
+                {upcomingMovies.map(movie => (
+                    <li key={movie.id}>
+                        {movie.id}, {movie.title}
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
+
+    return (
+        <div>
+            <h2>Movies</h2>
+            {moviesDisplay}
+            {upcomingMoviesDisplay}
+        </div>
+    );
+};
 
 export default MoviesPage;
